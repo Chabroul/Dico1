02c4537c34f5e35abcfb66b23e65c0c022c03e19
Cheberli Iyed <34692755+Chabroul@users.noreply.github.com>
2018-06-09 11:51:52 +0200

    Add files via upload

---
+++ dictionnaire/src/dictionnaire/Controleur/Test.java
@@ -0,0 +1,28 @@
+package dictionnaire.Controleur;
+//import org.apache.*;
+//import org.apache.commons.lang3.StringUtils;
+import dictionnaire.Modele.*;
+import dictionnaire.Vue.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import dictionnaire.Vue.MonCorrecteur;
+
+public class Test {
+	
+	
+	public static void main(String[] args) throws IOException 
+	{       
+	    System.setProperty("file.encoding","Fr-UTF-8");
+
+		MonCorrecteur Correc = new MonCorrecteur() ;
+	 
+	} 
+}
+	
+	
+	  
+	  
---
+++ dictionnaire/src/dictionnaire/Modele/Correcteur.java
@@ -0,0 +1,103 @@
+package dictionnaire.Modele;
+import dictionnaire.Vue.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Correcteur {
+
+	ArrayList<String> mots = new ArrayList<String>();
+	public Correcteur () throws IOException  
+	{	  	
+		BufferedReader lecteurAvecBuffer = null;
+	    String ligne;
+	    System.out.println(System.getProperty("file.encoding"));
+	
+	    try
+	      {
+	    	File fileDir = new File("mots.txt");
+	    	lecteurAvecBuffer =new BufferedReader(
+				   new InputStreamReader(
+		           new FileInputStream(fileDir), "UTF-8"));
+	      }
+	    catch(FileNotFoundException exc)
+	      {
+		System.out.println("Erreur d'ouverture");
+	      }
+	    while ((ligne = lecteurAvecBuffer.readLine()) != null)
+	    {
+	    	mots.add(ligne);
+	    }
+	    lecteurAvecBuffer.close();
+	    mots.sort(null);
+    } 
+	
+	public ArrayList<String> testerMot(String motTest) throws IOException
+	{
+		int i = 0 ;
+		ArrayList<String> motProbable = new ArrayList<String> () ;
+	    while (i < mots.size())
+	    {
+	    		if (mots.get(i).toLowerCase().equals(motTest.toLowerCase())) {motProbable.clear();motProbable.add("GOOD");break;}
+	    		if (LevenshteinDistance(motTest.toLowerCase(),mots.get(i).toLowerCase()) <= 2)	   		
+	    		if (motTest.charAt(0)==(mots.get(i).charAt(0)))
+	    			motProbable.add(mots.get(i)) ; 
+	    		i++ ;
+	    }
+	    i=0 ;
+	    return motProbable ;
+	}
+	    
+	  
+	
+	
+	public  int LevenshteinDistance(CharSequence lhs, CharSequence rhs)
+	{                          
+		    int len0 = lhs.length() + 1;                                                     
+		    int len1 = rhs.length() + 1;                                                     
+		                                                                                    
+		    // Tableau qui contiennent les distances                                                       
+		    int[] cost = new int[len0];                                                     
+		    int[] newcost = new int[len0];                                                  
+		                                                                                    
+		    for (int i = 0; i < len0; i++) cost[i] = i;                                     
+		                                                                                                                                                                    
+		    // Parcours des caractéres de la premiere chaine                                   
+		    for (int j = 1; j < len1; j++) {                                                
+		        newcost[0] = j;                                                             
+		                                                                                    
+		        // transformation cost for each letter in s0                                
+		        for(int i = 1; i < len0; i++) {                                             
+		            // Comparaison ds caracteres des deux mots                             
+		            int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;             
+		                                                                                    
+		                                         
+		            int cost_replace = cost[i - 1] + match;                                 
+		            int cost_insert  = cost[i] + 1;                                         
+		            int cost_delete  = newcost[i - 1] + 1;                                  
+		                                                                                    
+		            // On garde le cout minimum entre les deux mots                                                    
+		            newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+		        }                                                                           
+		                                                                                    
+		        // Reinitialiser les variables pour le prochain tour                                                 
+		        int[] swap = cost; cost = newcost; newcost = swap;                          
+		    }                                                                               
+		                                                                                    
+		    return cost[len0 - 1];                                                          
+		}
+	
+
+}
